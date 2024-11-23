ffe2d80186f407faf2f58daf001623922c80ceda
ETA ACADEMY <163610229+ETAAcademy@users.noreply.github.com>
2024-11-23 20:12:49 +0800

    49_zkbridge

---
+++ Appendix/sumcheck/src/lib.rs
@@ -0,0 +1,13 @@
+mod sumcheck;
+pub use sumcheck::*;
+
+mod prover_helper;
+
+mod verifier_helper;
+pub use verifier_helper::*;
+
+mod scratch_pad;
+pub use scratch_pad::{ProverScratchPad, VerifierScratchPad};
+
+mod utils;
+pub use utils::*;
